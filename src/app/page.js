c9a609ca1bb0aa610f8de757aa4a9f2b22bbccdd
Shivam Singh Kushwaha <98013830+shivamsk-creator@users.noreply.github.com>
2023-06-28 03:25:52 +0000

    ho gya

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-14">
-      <h2>Home Page</h2>
+      <h2>Home Page Dashboard</h2>
       <button onClick={() => router.push("/login")}>Go to Login Page</button>
       <button onClick={() => router.push("/about")}>Go to About Page</button>
       <Link href="/login">Login Page</Link>
